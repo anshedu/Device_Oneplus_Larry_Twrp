da5d98da72956bf0195522c3a0a3addcd81478eb
Ansh Agnihotri <167695711+anshedu@users.noreply.github.com>
2025-05-08 10:52:15 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-DEVICE_PATH := device/generic/boot
+DEVICE_PATH := device/larry/boot
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
